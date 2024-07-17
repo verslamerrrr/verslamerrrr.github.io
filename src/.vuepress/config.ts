df51b3c30ee4079b7f41e6f4d6e324e52c8375b0
verslamerrrr <verslamerrrr@gmail.com>
2024-07-17 23:32:48 +0800

    444

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -8,8 +8,8 @@ export default defineUserConfig({
   locales: {
     "/": {
       lang: "en-US",
-      title: "Blog Demo",
-      description: "A blog demo for vuepress-theme-hope",
+      title: "Fiona's Blog",
+      description: "A journey begins",
     },
     "/zh/": {
       lang: "zh-CN",
