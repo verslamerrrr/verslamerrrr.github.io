221b0d564e027d566a3a014d462c7de139372d29
verslamerrrr <verslamerrrr@gmail.com>
2024-08-04 17:45:32 +0800

    22

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -2,5 +2,11 @@ import { navbar } from "vuepress-theme-hope";
 
 export const zhNavbar = navbar([
   "/zh/",
+  {
+    text: "life",
+    icon: "",
+    link: "https://bornforthis.cn",
+
+  }
   
 ]);
