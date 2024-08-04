87450625887b43f76c082b89bafeebac3f54d0bf
verslamerrrr <verslamerrrr@gmail.com>
2024-08-04 18:34:30 +0800

    111

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -6,15 +6,31 @@ export const zhNavbar = navbar([
     text: "life",
     icon: "",
     children: [
-      "/zh/blog/page1.html",
-      "/zh/blog/page2.html",
-      "/zh/blog/page3.html",
-      "/zh/blog/page4.html",
-      "/zh/memoirs.html",
-      { text: "链接名称", icon: "图标", link: "/zh/life/look.html"}
+      {
+        text: "Group 1",
+        icon: "",
+        children: [
+          "/zh/home/blog.html",
 
+        ]
+      },
+      {
+        text: "Group 2",
+        icon: "",
+        children: [
+          "/zh/home/blog.html",
+
+        ]
+      }, {
+        text: "Group 2",
+        icon: "",
+        children: [
+          "/zh/home/blog.html",
+
+        ]
+      }
     ]
-
   }
+  
   
 ]);
